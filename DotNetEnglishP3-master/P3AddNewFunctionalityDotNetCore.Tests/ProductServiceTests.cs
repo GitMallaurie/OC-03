4f09b606dae1df2f1d057fac0a202d0cab17416f
Mallaurie <68752440+GitMallaurie@users.noreply.github.com>
2024-04-29 16:05:21 +0200

    test(ProductServiceTests) : change name from empty to default

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,17 +1,14 @@
-﻿using Xunit;
-using System.ComponentModel.DataAnnotations;
-using FluentAssertions;
-using System.Collections.Generic;
-using System.Linq;
+﻿using FluentAssertions;
 using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
     public class ProductServiceTests
     {
-        private readonly ProductViewModel _productViewModel;
-
-        private ProductViewModel CreateEmptyProduct(string name = " ", string stock = " ", string price = " ")
+        private ProductViewModel CreateDefaultProduct(string name = "Default", string stock = "1", string price = "1")
         {
             return new ProductViewModel
             {
@@ -27,7 +24,7 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         public void Name_WhenMissing_ShouldReturnErrorMessage(string MissingName)
         {
             // Arrange 
-            var product = CreateEmptyProduct(name : MissingName);
+            var product = CreateDefaultProduct(name: MissingName);
 
             // Act
             var context = new ValidationContext(product);
@@ -45,7 +42,7 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         public void Price_WhenMissing_ShouldReturnErrorMessage(string MissingPrice)
         {
             // Arrange 
-            var product = CreateEmptyProduct(price : MissingPrice);
+            var product = CreateDefaultProduct(price: MissingPrice);
 
             // Act
             var context = new ValidationContext(product);
@@ -63,7 +60,7 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         public void Stock_WhenMissing_ShouldReturnErrorMessage(string MissingStock)
         {
             // Arrange 
-            var product = CreateEmptyProduct(stock : MissingStock);
+            var product = CreateDefaultProduct(stock: MissingStock);
 
             // Act
             var context = new ValidationContext(product);
@@ -81,7 +78,7 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         public void Price_WhenNotANumber_ShouldReturnErrorMessage(string PriceNotANumber)
         {
             // Arrange
-            var product = CreateEmptyProduct(price : PriceNotANumber) ;
+            var product = CreateDefaultProduct(price: PriceNotANumber);
 
             // Act
             var context = new ValidationContext(product);
@@ -99,7 +96,7 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         public void Stock_WhenNotAnInteger_ShouldReturnErrorMessage(string StockNotAnInteger)
         {
             // Arrange
-            var product = CreateEmptyProduct(stock : StockNotAnInteger);
+            var product = CreateDefaultProduct(stock: StockNotAnInteger);
 
             // Act
             var context = new ValidationContext(product);
@@ -119,7 +116,7 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         public void Stock_NotGreaterThanZero_ShouldReturnErrorMessage(string StockNotGreaterThanZero)
         {
             // Arrange
-            var product = CreateEmptyProduct(stock : StockNotGreaterThanZero);
+            var product = CreateDefaultProduct(stock: StockNotGreaterThanZero);
 
             // Act
             var context = new ValidationContext(product);
@@ -137,7 +134,7 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         public void Price_NotGreaterThanZero_ShouldReturnErrorMessage(string PriceNotGreaterThanZero)
         {
             // Arrange
-            var product = CreateEmptyProduct(price: PriceNotGreaterThanZero);
+            var product = CreateDefaultProduct(price: PriceNotGreaterThanZero);
 
             // Act
             var context = new ValidationContext(product);
@@ -150,18 +147,18 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         }
 
         [Theory]
-        [InlineData("1,99", 1.99)] 
+        [InlineData("1,99", 1.99)]
         [InlineData("1.99", 1.99)]
         public void Price_WithDecimalSeparators_ShouldBeValid(string inputPrice, double expectedPrice)
         {
             // Arrange
-            var product = CreateEmptyProduct(price: inputPrice);
+            var product = CreateDefaultProduct(price: inputPrice);
 
             // Act
             var actualPrice = product.PriceForValidation;
 
             // Assert
-            actualPrice.Should().Be(expectedPrice); 
+            actualPrice.Should().Be(expectedPrice);
         }
     }
 }
